005e0d4b888460eef9557225255ce1dadd31f6a5
TomSif <thomas.sifferle.programmer@gmail.com>
2026-03-11 12:34:06 +0100

    feat(calculator): coonvertToMetric , calculateBMI, resetStates, onFocus and healthy range

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -2,14 +2,51 @@ import { useState } from "react";
 
 function Calculator() {
   const [unity, setUnity] = useState<"metric" | "imperial">("metric");
-  const [height, setHeight] = useState<number | null>(null);
-  const [weight, setWeight] = useState<number | null>(null);
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+  const [feet, setFeet] = useState<number>(0);
+  const [inches, setInches] = useState<number>(0);
+  const [stones, setStones] = useState<number>(0);
+  const [lbs, setLbs] = useState<number>(0);
+
+  function resetStates() {
+    setHeight(0);
+    setWeight(0);
+    setFeet(0);
+    setInches(0);
+    setStones(0);
+    setLbs(0);
+  }
+
+  function convertToMetric(
+    feet: number,
+    inches: number,
+    stones: number,
+    lbs: number,
+  ) {
+    const h = feet * 30.48 + inches * 2.54;
+    const w = stones * 6.35 + lbs * 0.453;
+    return { height: h, weight: w };
+  }
 
   function calculateBMI() {
-    if (weight && height) {
-      return weight / ((height / 100) * (height / 100));
-    } else return null;
+    if (unity === "metric") {
+      if (weight > 0 && height > 0) {
+        return weight / ((height / 100) * (height / 100));
+      } else return 0;
+    } else {
+      if (feet > 0 && stones > 0) {
+        const { height: h, weight: w } = convertToMetric(
+          feet,
+          inches,
+          stones,
+          lbs,
+        );
+        return w / ((h / 100) * (h / 100));
+      } else return 0;
+    }
   }
+
   const bmi = calculateBMI();
 
   function getBMICategory(bmi: number) {
@@ -24,12 +61,21 @@ function Calculator() {
       return "";
     }
   }
+
+  function getHealthyWeightRange(height: number) {
+    const min = 18.5 * ((height / 100) * (height / 100));
+    const max = 24.9 * ((height / 100) * (height / 100));
+
+    return { min, max };
+  }
+  const range = height > 0 ? getHealthyWeightRange(height) : null;
+
   return (
     <div className="bg-grey-500 w-54 h-54">
       <form>
         <fieldset>
           <legend>Enter your details below</legend>
-          <div className="flex ">
+          <div className="flex w-full">
             <label htmlFor="metric">Metric</label>
             <input
               checked={unity === "metric"}
@@ -39,6 +85,7 @@ function Calculator() {
               value="metric"
               onChange={() => {
                 setUnity("metric");
+                resetStates();
               }}
             />
             <label htmlFor="imperial">Imperial</label>
@@ -50,38 +97,119 @@ function Calculator() {
               value="imperial"
               onChange={() => {
                 setUnity("imperial");
+                resetStates();
               }}
             />
           </div>
-          <div className="flex">
-            <label htmlFor="height">Height</label>
-            <input
-              type="number"
-              min="0"
-              max="300"
-              id="height"
-              name="height"
-              value={height ?? ""}
-              onChange={(e) => {
-                setHeight(Number(e.target.value));
-              }}
-            />
-            <label htmlFor="weight">Weight</label>
-            <input
-              type="number"
-              min="0"
-              max="300"
-              id="weight"
-              name="weight"
-              value={weight ?? ""}
-              onChange={(e) => {
-                setWeight(Number(e.target.value));
-              }}
-            />
-          </div>
+          {unity === "metric" ? (
+            <div className="flex">
+              <div className="flex flex-col w-full">
+                <label htmlFor="height">Height</label>
+                <input
+                  className="bg-white"
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="height"
+                  name="height"
+                  value={height}
+                  onChange={(e) => {
+                    setHeight(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <label htmlFor="weight">Weight</label>
+                <input
+                  className="bg-white"
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="weight"
+                  name="weight"
+                  value={weight}
+                  onChange={(e) => {
+                    setWeight(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col w-full">
+              <label htmlFor="feet">Height</label>
+              <div className="flex w-full">
+                <input
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="feet"
+                  name="feet"
+                  value={feet}
+                  onChange={(e) => {
+                    setFeet(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+                <input
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="inches"
+                  name="inches"
+                  value={inches}
+                  onChange={(e) => {
+                    setInches(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+              </div>
+              <label htmlFor="stones">Weight</label>
+              <div className="flex w-full ">
+                <input
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="stones"
+                  name="stones"
+                  value={stones}
+                  onChange={(e) => {
+                    setStones(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+                <input
+                  type="number"
+                  min="0"
+                  max="300"
+                  id="lbs"
+                  name="lbs"
+                  value={lbs}
+                  onChange={(e) => {
+                    setLbs(Number(e.target.value));
+                  }}
+                  onFocus={(e) => e.target.select()}
+                />
+              </div>
+            </div>
+          )}
         </fieldset>
-        <p>{bmi ? bmi.toFixed(2) : ""}</p>
-        <p>{bmi ? getBMICategory(bmi) : ""}</p>
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col">
+            <p>{bmi ? bmi.toFixed(2) : ""}</p>
+            <p>{bmi ? getBMICategory(bmi) : ""}</p>
+          </div>
+          <div>
+            <p>
+              Your BMI suggests you’re a healthy weight. Your ideal weight is
+              between
+              <strong>
+                {range?.min.toFixed(1)} kgs - {range?.max.toFixed(1)} kgs
+              </strong>
+            </p>
+          </div>
+        </div>
       </form>
     </div>
   );
